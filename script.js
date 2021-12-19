3002b1c5e048a1007288e61025f0d2ed583f5fe2
Benjamin <benjaminshh@gmail.com>
2021-12-19 13:42:13 -0800

    Added a ui

--- script.js
+++ script.js
@@ -32,25 +32,82 @@ function playRound(playerSelection,computerSelection){
             return("You Win! Scissors beats Paper!")
     }
 }
+function score(result){
+    if(result.search("Win") != -1) {
+        console.log(result.search("Win") != -1)
+        playerScore++;
+    }
+    if(result.search("Lose") != -1){
+        console.log(result.search("Lose") != -1)
+        compScore++;
+    }
+}
+const body = document.querySelector('body');
+
+const buttons = document.querySelectorAll('button');
+const div = document.createElement('div');
+const scoreTracker = document.createElement('div');
+const winner = document.createElement('div');
+let playerScore,compScore;
+playerScore = compScore = 0;
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+        result = playRound(button.id,computerPlay());
+        score(result);
+        div.textContent = result;
+        scoreTracker.textContent = `You currently have ${playerScore} wins,
+                                    the computer has ${compScore} wins.`
+        if(playerScore == 5 || compScore == 5){
+            playerScore == 5 ? winner.textContent = "Congrats, you win!" :
+            winner.textContent = "Aw, you lost! Better luck next time!";
+        }
+    })
+});
+
+body.appendChild(div);
+body.appendChild(scoreTracker);
+body.appendChild(winner);
+if(playerScore == 5 || compScore == 5){
+    playerScore == 5 ? winner.textContent = "Congrats, you won the match!" :
+    winner.textContent = "Aw, you lost! Better luck next time!";
+}
+/* Writing event listeners separately for each button
+const rock = document.querySelector('#Rock');
+rock.addEventListener('click', function(){
+    const rock_result = document.createElement('div');
+    rock_result.textContent = playRound("Rock",computerPlay());
+    body.appendChild(rock_result);
+})
+const paper = document.querySelector('#Paper');
+paper.addEventListener('click',function(){
+    console.log(playRound("Paper",computerPlay()))
+})
+const scissors = document.querySelector('#Scissors');
+scissors.addEventListener('click',function(){
+    console.log(playRound("Scissors",computerPlay()))
+})
+*/
 function game(){
     let playerScore = 0
     let computerScore = 0
     for(i = 0; i < 5; i++){
-    playersHand = window.prompt("Rock, Paper, or Scissors?")
-    let result = playRound(playersHand,computerPlay())
-    console.log(result);
-    if(result.search("Tie") != -1){
-        i--
-    }
-    if(result.search("Win") != -1){
-        playerScore++
-    }
-    }
-    if(playerScore > 2){
-        return(`You Won w/ ${playerScore} wins and ${5 - playerScore}
-        losses!`)
-    }
-    else{
-        return(`You lost w/ ${playerScore} wins and ${5 - playerScore} losses!`)
+        playersHand = window.prompt("Rock, Paper, or Scissors?")
+        let result = playRound(playersHand,computerPlay())
+        console.log(result);
+        if(result.search("Tie") != -1){
+            i--
+        }
+        if(result.search("Win") != -1){
+            playerScore++
+        }
+        }
+        if(playerScore > 2){
+            return(`You Won w/ ${playerScore} wins and ${5 - playerScore}
+            losses!`)
+        }
+        else{
+            return(`You lost w/ ${playerScore} wins and ${5 - playerScore} losses!`)
     }
 }
